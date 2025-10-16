f3e306d2fef72730cd44dc121ff376083bf41739
Juan Escobar <37384155+jcescobarn@users.noreply.github.com>
2025-10-15 19:09:37 -0500

    fix

--- main_test.py
+++ main_test.py
@@ -1,13 +1,20 @@
-from main import get_weather
 import unittest
-from unittest.mock import patch
 
-class Test(unittest, TestCase):
-    @patch("main.request.get")
-    def test_get_weather(mock_get):
-        mock_get.return_value.json.return_value = {"temperature": 22}
-        result = get_weather()
-        self.assertTrue(result, 22)
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
